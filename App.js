05da9c5983d112e84629c50e94138bc825b6691c
StrawCraft <daniel.isaach@student.umn.ac.id>
2025-02-07 11:10:00 +0700

    week1

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <ScrollView>
     <View style={styles.container}>
-      <Image source = {require('./assets/sponge.png')} />
-      <Text>Spongebob Squarepants</Text>
-      <Text>0000001</Text>
-      <Image source = {require('./assets/Pat.png')} />
-      <Text>Patrick Star</Text>
-      <Text>0000002</Text>
-      <Image source = {require('./assets/Squid.png')} />
-      <Text>Squidward Tentacle</Text>
-      <Text>0000003</Text>
-      <Image source = {require('./assets/Sandy.png')} />
-      <Text>Sandy Squirell</Text>
-      <Text>0000004</Text>
-      <Image source = {require('./assets/Crab.png')} />
-      <Text>Eugine Crab</Text>
-      <Text>0000005</Text>
-      <Image source = {require('./assets/Plank.png')} />
-      <Text></Text>
+      <Text>Daniel Isaach Hatuaon Siregar - 00000078609</Text>
       <StatusBar style="auto" />
     </View>
-    </ScrollView>
   );
 }
 
