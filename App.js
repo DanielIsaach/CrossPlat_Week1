264a44df87528a4f4e90b75b39ea477a51414f58
StrawCraft <daniel.isaach@student.umn.ac.id>
2025-02-07 10:16:34 +0700

    Week 1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Daniel Isaach Hatuaon Siregar - 00000078609</Text>
       <StatusBar style="auto" />
     </View>
   );
